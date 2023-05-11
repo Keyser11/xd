68c88b0fd4a32cb068a89fc2b7867c59277d884c
Mariobrosh44 <128196730+Mariobrosh44@users.noreply.github.com>
2023-05-11 03:36:51 -0500

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,13 @@
+
+function CambiaMsj() {
+  document.getElementById("loading-msj").innerText = "Listo!! ✅"
+}
+setTimeout(CambiaMsj,6000)
+
+
+setTimeout(function() {
+  document.getElementById("Btn-Comenzar").style.display = "block";
+}, 7000);
+
+
+
